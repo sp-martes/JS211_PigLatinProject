f668da415886995e4c5c5afd20c165a24c7768fe
sp-martes <spencermartes@gmail.com>
2021-07-06 18:50:45 -0500

    finally got it

--- main.js
+++ main.js
@@ -22,36 +22,31 @@ const pigLatin = (word) => {
   let i= vowelIndex(w);
 
     if(i === 0) {
-      return w += 'yay'
+      return w += 'yay '
     }
     else if(i === -1){
-      return w += 'ay'
+      return w += 'ay '
     }
     else{
-      return w.slice(i) + w.slice(0,i) + 'ay'
+      return w.slice(i) + w.slice(0,i) + 'ay '
     }
 }  
 
-// const pigArray = (words) => {
-//   let inputArray = words.split(" ");
-//   console.log(inputArray.length);
-//   for(let i=0; i < inputArray.length; i++){
-//     let loop= inputArray[i];
-
-//        console.log( 'array length:', loop);
-//     return pigLatin(loop);
-//   }
-// }
-
-
-
-
+const pigArray = (words) => {
+  let inputArray = words.split(" ");
+  let sentence = "";
+  for(const arrayWords of inputArray){
+     sentence += pigLatin(arrayWords);
+  }
+  return sentence;
+  }
+  
 // the first function called in the program to get an input from the user
 // to run the function use the command: node main.js
 // to close it ctrl + C
 const getPrompt = () => {
   rl.question('word ', (answer) => {
-    console.log( pigLatin(answer) );
+    console.log( pigArray(answer) );
     getPrompt();
   });
 }
