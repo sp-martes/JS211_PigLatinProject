19b3868738f35e01f2689ade8f70530d1075c4a3
sp-martes <spencermartes@gmail.com>
2021-07-14 17:24:02 -0500

    tinkered around a little

--- main.js
+++ main.js
@@ -1,26 +1,21 @@
 'use strict';
 
-// brings in the assert module for unit testing
-const assert = require('assert');
-// brings in the readline module to access the command line
-const readline = require('readline');
-// use the readline module to print out to the command line
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
 
 const pigLatin = (word) => {
   
   let w = word.trim().toLowerCase();
   
+  if ((w[w.length-1] === ".") || (w[w.length-1] === "!") ||
+      (w[w.length-1] === "?")){
+    w = w.slice(0,-1);
+  }
+    
   let vowel1 = w.match(/[aeiou]/);
    
   let i= w.indexOf(vowel1);
 
     if(i === 0) {
-      return w += 'yay '
+      return w += 'yay ' 
     }
     else if(i === -1){
       return w += 'ay '
@@ -34,48 +29,22 @@ const pigArray = (words) => {
   let inputArray = words.split(" ");
   let sentence = "";
   for(const arrayWords of inputArray){
-     sentence += pigLatin(arrayWords);
+    sentence += pigLatin(arrayWords);
   }
-  return sentence;
-  }
+  let capital = sentence[0].toUpperCase() + sentence.slice(1)
+  return capital;
+}
+
+const pigGUI = () =>{
   
-// the first function called in the program to get an input from the user
-// to run the function use the command: node main.js
-// to close it ctrl + C
-const getPrompt = () => {
-  rl.question('word ', (answer) => {
-    console.log( pigArray(answer) );
-    getPrompt();
-  });
-}
-
-// Unit Tests
-// to use them run the command: npm test main.js
-// to close them ctrl + C
-if (typeof describe === 'function') {
-
-  describe('#pigLatin()', () => {
-    it('should translate a simple word', () => {
-      assert.equal(pigLatin('car'), 'arcay');
-      assert.equal(pigLatin('dog'), 'ogday');
-    });
-    it('should translate a complex word', () => {
-      assert.equal(pigLatin('create'), 'eatecray');
-      assert.equal(pigLatin('valley'), 'alleyvay');
-    });
-    it('should attach "yay" if word begins with vowel', () => {
-      assert.equal(pigLatin('egg'), 'eggyay');
-      assert.equal(pigLatin('emission'), 'emissionyay');
-    });
-    it('should lowercase and trim word before translation', () => {
-      assert.equal(pigLatin('HeLlO '), 'ellohay');
-      assert.equal(pigLatin(' RoCkEt'), 'ocketray');
-    });
-  });
-} else {
-
-  getPrompt();
-
+  let tBox = document.querySelector('#translateBox')
+  let after = pigArray(tBox.value); 
+ 
+  let p = document.createElement("p")
+  let div = document.getElementById("result")
+  div.appendChild(p);
+  p.innerText = after;
+  tBox.value = "";
 }
 
 
@@ -83,11 +52,6 @@ if (typeof describe === 'function') {
 
 
 
-// **********
-//   HINTS
-// **********
 
-// break your code into pieces and focus on one piece at a time...
-// 1. if word begins with a vowel send to one function: adds "yay"
-// 2. if word begins with a consonant send to another function: splices off beginning, returns word with new ending.
-// 3. if multiple words, create array of words, loop over them, sending them to different functions and creating a new array with the new words.
+
+
